5c5262c2713284a603d0499e455f068e8b70b27e
Khadiime7 <142298858+Khadiime7@users.noreply.github.com>
2023-11-26 23:59:38 +0400

    Update webapp_streamlit.py

--- webapp_streamlit.py
+++ webapp_streamlit.py
@@ -87,9 +87,10 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     predictions = model.predict(import_and_predict(image))
-    print(predictions)
-    x = random.randint(98,99)+ random.randint(0,99)*0.01
-    st.sidebar.error("Accuracy : " + str(x) + " %")
+    predictions = sorted(predictions)
+    # print(predictions)
+    # x = random.randint(98,99)+ random.randint(0,99)*0.01
+    st.sidebar.error("Accuracy : " + str(predictions[-1]) + " %")
 
     class_names = ['Cyst','Normal','Stone','Tumor']
 

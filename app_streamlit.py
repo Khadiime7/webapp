54f03f072e8c74e8682645978515a118329ca6e7
Khadiime7 <142298858+Khadiime7@users.noreply.github.com>
2023-11-26 11:21:08 +0400

    Add files via upload

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,34 +1,38 @@
 import streamlit as st
-import torch
-import torchvision.transforms as transforms
-from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50, decode_predictions, preprocess_input
+from tensorflow.keras.preprocessing import image
 from lime import lime_image
+import numpy as np
 
-# Load the pretrained ResNet model
-resnet_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
-resnet_model.eval()
+# Load the pre-trained ResNet50 model
+resnet_model = ResNet50(weights='imagenet')
 
-# Define the transformation
-preprocess = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
-# Define the explain function using Lime
-def explain(image):
-    # Preprocess the image
-    input_tensor = preprocess(image).unsqueeze(0)
+# Define the explain function
+def explain(image_path):
+    # Load and preprocess the image
+    img = image.load_img(image_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
 
     # Get model predictions
-    output = resnet_model(input_tensor)
+    predictions = resnet_model.predict(img_array)
+    decoded_predictions = decode_predictions(predictions, top=3)[0]
 
     # Create a Lime explainer
     explainer = lime_image.LimeImageExplainer()
 
     # Explain the image
-    explanation = explainer.explain_instance(input_tensor[0].permute(1, 2, 0).numpy(), resnet_model.predict, top_labels=1, hide_color=0, num_samples=1000)
+    explanation = explainer.explain_instance(
+        img_array[0],
+        resnet_model.predict,
+        top_labels=1,
+        hide_color=0,
+        num_samples=1000
+    )
 
-    return explanation
+    return decoded_predictions, explanation
 
 # Streamlit app
 st.title("Explainable AI Web App")
@@ -36,16 +40,23 @@ st.title("Explainable AI Web App")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    # Preprocess the uploaded image
-    image = Image.open(uploaded_file)
+    # Save the uploaded image
+    with open("temp_image.jpg", "wb") as f:
+        f.write(uploaded_file.getvalue())
 
     # Explain the image
-    explanation = explain(image)
+    predictions, lime_explanation = explain("temp_image.jpg")
 
     # Display the original image
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-    # Display the explanation
-    st.image(explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5)[0], caption="Explanation", use_column_width=True)
+    # Display the model predictions
+    st.subheader("Model Predictions:")
+    for i, (imagenet_id, label, score) in enumerate(predictions):
+        st.write(f"{i + 1}: {label} ({score:.2f})")
+
+    # Display the Lime explanation
+    st.subheader("Lime Explanation:")
+    st.image(lime_explanation.image, caption="Explanation", use_column_width=True)
 
     st.success("Explanation generated!")
